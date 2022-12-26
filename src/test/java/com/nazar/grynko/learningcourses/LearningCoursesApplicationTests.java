2fa4629e1431ce3508fc4aaa417a216d2f0638b5
AlwaysSayNo <allbitxxx@gmail.com>
2022-12-26 11:38:30 +0200

    Initial commit

---
+++ src/test/java/com/nazar/grynko/learningcourses/LearningCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazar.grynko.learningcourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
