e3edb430a5f325fbe5ad8970dc99041c63fe630e
Admin <ML31feed>
2023-11-14 20:28:03 +0200

    [CORS] (CORS integrate with MvcConfigurer)

--- src/main/java/com/nazar/grynko/learningcourses/configuration/MvcConfiguration.java
+++ src/main/java/com/nazar/grynko/learningcourses/configuration/MvcConfiguration.java
@@ -4,6 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @ComponentScan("com.nazar.grynko.learningcourses")
@@ -12,6 +18,16 @@ public class MvcConfiguration {
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://learning-courses-production.up.railway.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
